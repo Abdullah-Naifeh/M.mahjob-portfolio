3bca966454c14af9fd80b8affddf7c848c93b46b
unknown <abdna59@gmail.com>
2024-01-23 19:00:11 +0300

    creat first page

--- my-app/app/page.js
+++ my-app/app/page.js
@@ -4,7 +4,19 @@ export default function Home() {
   return (
     <div>
       <Navbar/>
-      <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <main className="flex h-[91vh]">
+        <div className="w-[30%] bg-slate-800">
+          <img src=""></img>
+        </div>
+        <div className="w-[70%]">
+          <div className="flex flex-col justify-around items-start p-9 m-9">
+            <h5 className="text-[14px] leading-5 font-semibold md:text-[18px]  ">HELLO, THERE</h5>
+            <h1>I AM MOHAMMED MAHJOUB</h1>
+            <button>
+              DOWNLOAD CV
+            </button>
+          </div>
+        </div>
       </main>
     </div>
   );
--- my-app/components/Navbar.jsx
+++ my-app/components/Navbar.jsx
@@ -1,23 +1,52 @@
-export default function Navbar(){
-    return(
-        <nav className="flex justify-between bg-[#FFC815] fixed top-0 right-0 w-[300px] min-h-[60px]">
-            <div className="py-1 px-4">
-                <h1 className="text-white text-xl font-semibold leading-[50px]">LOGO</h1>
-            </div>
-            <div className="hidden">
-                <ul className="flex flex-col md:flex-row">
-                    <li><a href="http://">HOME</a></li>
-                    <li><a href="http://">ABOUT US</a></li>
-                    <li><a href="http://">RESUME</a></li>
-                    <li><a href="http://">PORTFOLIO</a></li>
-                    <li><a href="http://">CONTACT ME</a></li>
-                </ul>
-            </div>
-            <div className=" flex flex-col justify-center items-center bg-black w-[60px] h-[60px] cursor-pointer">
-                <span className="bg-white w-6 h-[2px]"></span>
-                <span className="bg-white w-9 h-[2px] my-2"></span>
-                <span className="bg-white w-6 h-[2px]"></span>
-            </div>
-        </nav>
-    )
+"use client";
+
+import { useState } from "react";
+
+export default function Navbar() {
+  const [clicked, setClicked] = useState(false);
+  return (
+    <nav className="flex justify-between items-center bg-[#FFC815] ">
+      <div className="py-1 px-4">
+        <h1 className="text-white text-xl font-semibold leading-[50px]">
+          LOGO
+        </h1>
+      </div>
+      
+        <div>
+          <ul className="flex flex-col md:flex-row justify-around text-white text-[16px]">
+            <li className="md:mr-8">
+              <a href="http://">HOME</a>
+            </li>
+            <li className="md:mr-8">
+              <a href="http://">ABOUT US</a>
+            </li>
+            <li className="md:mr-8">
+              <a href="http://">RESUME</a>
+            </li>
+            <li className="md:mr-8">
+              <a href="http://">PORTFOLIO</a>
+            </li>
+            <li className="md:mr-8">
+              <a href="http://">CONTACT ME</a>
+            </li>
+          </ul>
+        </div>
+      
+
+      {/* <button
+        onClick={() => {
+            if(clicked == false ){
+                setClicked(true)
+            }else{
+                setClicked(false)
+            }
+        }}
+        className=" flex flex-col justify-center items-center bg-black w-[60px] h-[60px] cursor-pointer"
+      >
+        <span className="bg-white w-6 h-[2px]"></span>
+        <span className="bg-white w-9 h-[2px] my-2"></span>
+        <span className="bg-white w-6 h-[2px]"></span>
+      </button> */}
+    </nav>
+  );
 }
